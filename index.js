19b43b4d8db009127fc0e86c9aba246bf1153c8f
Jesus Rojas <jesus@invorious.com>
2024-04-04 13:38:06 -0500

    ci: add workflow of github actions :construction_worker:

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import random from 'random'
-import simpleGit from 'simple-git'
-import jsonfile from 'jsonfile'
-import moment from 'moment'
+import random from 'random';
+import simpleGit from 'simple-git';
+import jsonfile from 'jsonfile';
+import moment from 'moment';
 
-const filePath = './data.json'
+const filePath = './data.json';
 
-const makeCommit = (contador) => {
-  if(contador === 0) return simpleGit().push()
-  const weeks = random.int(0, 45)
-  const days = random.int(0, 50)
-  const date = moment().subtract(1, 'y').add(1, 'd').add(weeks, 'w').add(days, 'd').format()
-  const data = { date }
+export const makeCommit = (contador) => {
+  if(contador === 0) return simpleGit().push();
+  const weeks = random.int(0, 45);
+  const days = random.int(0, 50);
+  const date = moment()
+    .subtract(1, 'y')
+    .add(1, 'd')
+    .add(weeks, 'w')
+    .add(days, 'd')
+    .format();
 
-  console.log(`contador: ${contador}`)
-  
-  jsonfile.writeFile(filePath, data, () => {
-    simpleGit().add(filePath).commit(date, { '--date': date }, makeCommit.bind(this, --contador))
-  })
-}
-
-export {
-  makeCommit
-}
+  jsonfile.writeFile(filePath, { date }, () => {
+    simpleGit()
+      .add(filePath)
+      .commit(`[bot] ${date}`, { '--date': date }, makeCommit.bind(this, --contador));
+  });
+};
---
+++ main.js
@@ -0,0 +1,3 @@
+import { makeCommit } from './index';
+
+makeCommit(10);
